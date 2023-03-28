97e8e8ff4703930e6e4501992ff1b529e3d94511
⁨DRACULA⁩ <122371652+DRACULA-22@users.noreply.github.com>
2023-03-29 01:48:55 +0300

    Add files via upload

--- Config.py
+++ Config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = int(os.environ.get("APP_ID", "18943318"))
+    API_HASH = os.environ.get("API_HASH", "f00b7def1f034b90a866d382078bd68f")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "DRACULA_T22BOT")
     SUPPORT = os.environ.get("SUPPORT", "DRACULA_SUPPORT")
     CHANNEL = os.environ.get("CHANNEL", "DRACULA")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2ad68bd0e391a69163d0a.jpg")
